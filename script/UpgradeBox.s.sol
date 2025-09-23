c780f5296100511b5de6483859d1620eb64f2161
Loc Giang <locbgiang@gmail.com>
2025-09-22 20:09:57 -0700

    UpgradeBox"

--- script/UpgradeBox.s.sol
+++ script/UpgradeBox.s.sol
@@ -8,8 +8,22 @@ import { ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.
 import { DevOpsTools } from "@foundry-devops/src/DevOpsTools.sol";
 
 contract UpgradeBox is Script {
-    function run () {
-        
+    function run () external returns (address) {
+        address mostRecentlyDeployedProxy = DevOpsTools.get_most_recent_deployment("ERC1967Proxy", block.chainid);
+
+        vm.startBroadcast();
+        BoxV2 newBox = new BoxV2();
+        vm.stopBroadcast();
+        address proxy = upgradeBox(mostRecentlyDeployedProxy, address(newBox));
+        return proxy;
+    }
+
+    function upgradeBox(address proxyAddress, address newBox) public returns (address) {
+        vm.startBroadcast();
+        BoxV1 proxy = BoxV1(payable(proxyAddress));
+        proxy.upgradeToAndCall(address(newBox), "");
+        vm.stopBroadcast();
+        return address(proxy);
     }
 }
 
